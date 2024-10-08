9632c23e27f3aaaddee0d7d4df201a84193ffd6a
bisratawoke <bisrat@360ground.com>
2024-10-08 15:31:41 +0300

    Object list endpoint

--- src/modules/publication/publication.controller.ts
+++ src/modules/publication/publication.controller.ts
@@ -1,8 +1,26 @@
-import { Body, Controller, Delete, Get, Param, Post, Query, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Query,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { MinioService } from 'src/minio/minio.service';
+import { PublicationService } from './publication.service';
 
 @Controller('publications')
 export class PublicationController {
+  constructor(private readonly publicationService: PublicationService) {}
 
+  @Get('vs/:bucketName')
+  async getAll(@Param('bucketName') bucketName: string) {
+    const list = await this.publicationService.listFiles(bucketName);
+    return list;
+  }
 }
--- src/modules/publication/publication.module.ts
+++ src/modules/publication/publication.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PublicationController } from './publication.controller';
 import { PublicationService } from './publication.service';
+import { MinioService } from 'src/minio/minio.service';
 
 @Module({
   controllers: [PublicationController],
-  providers: [PublicationService]
+  providers: [PublicationService, MinioService],
 })
 export class PublicationModule {}
