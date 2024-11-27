1b5962b45a5d6e93efaa652b74892a15bd93ee01
bisratawoke <bisratawoke52@gmail.com>
2024-11-27 10:52:02 +0300

    wip

--- src/pipes/objectIdvalidation.pipe.ts
+++ src/pipes/objectIdvalidation.pipe.ts
@@ -4,11 +4,12 @@ import {
   PipeTransform,
 } from '@nestjs/common';
 import { ObjectId } from 'mongodb';
+import mongoose from 'mongoose';
 
 export default class ObjectIdValidationPipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
     try {
-      return new ObjectId(value);
+      return new mongoose.Types.ObjectId(value);
     } catch (err) {
       throw new BadRequestException(
         `The value provided for ${metadata.data} is not a vaild object id`,
